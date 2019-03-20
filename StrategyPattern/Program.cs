7bb6f54b4e4224b163c9747ad7863c56552afc22
AlexandrTolstov <alextav_85@mail.ru>
2019-03-20 14:29:12 +0600

    1

--- StrategyPattern/Program.cs
+++ StrategyPattern/Program.cs
@@ -10,6 +10,44 @@ namespace StrategyPattern
     {
         static void Main(string[] args)
         {
+            Car auto = new Car(4, "Volvo", new PetrolMove());
+            auto.Move();
+            auto.Movable = new ElectricMove();
+            auto.Move();
+        }
+        interface IMovable
+        {
+            void Move();
+        }
+        class PetrolMove : IMovable
+        {
+            public void Move()
+            {
+                Console.WriteLine("Перемещение на бензине");
+            }
+        }
+        class ElectricMove : IMovable
+        {
+            public void Move()
+            {
+                Console.WriteLine("Перемещение на электричесве");
+            }
+        }
+        class Car
+        {
+            protected int passengers;
+            protected string model;
+            public IMovable Movable { private get; set; }
+            public Car(int num, string model, IMovable mov)
+            {
+                this.passengers = num;
+                this.model = model;
+                Movable = mov;
+            }
+            public void Move()
+            {
+                Movable.Move();
+            }
         }
     }
 }
